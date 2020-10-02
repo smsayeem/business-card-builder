da7e35726e94ef0c4f4c1d4d237765d5e31f82d3
Sam Sadot <sam.sadot@sca.com.au>
2020-10-02 13:23:27 +1000

    add CardPreview component

--- src/components/CardPreview/index.js
+++ src/components/CardPreview/index.js
@@ -1,14 +1,52 @@
-import React, { Component } from 'react';
+import React from 'react';
+import FallBackImg from '../Assets/sample_fallback.png';
 import './style.css';
 
-export class CardPreview extends Component {
-  render() {
-    return (
-      <div>
-      CardPreview
-      </div>
-    )
-  }
+const CardPreview = ({user, avatar: { preview_imgUrl }}) => {
+  const {email, phone, houseno, street, suburb, state, postcode, country} = user;
+  
+  return (
+    <section className="preview">
+          <h1 className="card-title">hCard preview</h1>
+          {/* preview card header */}
+          <header className="card-header">
+            <h2>sam fairfax</h2>
+            <div className="card-avatar">
+              <img src={ preview_imgUrl || FallBackImg } alt="avatar"/>
+            </div>
+          </header>
+           {/* preview card body */}
+          <section className="card-body">
+              <div className="card-items">
+                <div className="label">email</div>
+                <div className="info">{email}</div>
+              </div>
+              <div className="card-items">
+                <div className="label">phone</div>
+                <div className="info">{phone}</div>
+              </div>
+              <div className="card-items">
+                <div className="label">address</div>
+                <div className="info">{houseno} {street}</div>
+              </div>
+              <div className="card-items">
+                <div style={{visibility: 'hidden'}} className="label">address2</div>
+                <div className="info">{suburb ? suburb + ',' : null} {state}</div>
+              </div>
+              {/* bottom two items sits next to each other, added a special wrapper */}
+              <div className="card-special-item-wrapper">
+                  <div className="card-special-items">
+                    <div className="label">postcode</div>
+                    <div className="info-special">{postcode}</div>
+                  </div>
+                  <div  className="card-special-items">
+                    <div className="label">country</div>
+                    <div className="info-special">{country}</div>
+                  </div>
+              </div>
+          </section>
+    </section>
+  )
 }
 
-export default CardPreview
+export default CardPreview;
